59f953bb00e3585fddd8b79b07d2af33c84a877d
niraj <niraj.aswani@klevu.com>
2017-01-17 12:32:41 +0530

    initial version after restructuring

---
+++ Block/Search/Index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © 2015 Dd . All rights reserved.
+ */
+namespace Klevu\Search\Block\Search;
+class Index extends \Magento\Framework\View\Element\Template
+{
+	public function _prepareLayout()
+	{
+	   return parent::_prepareLayout();
+	}  
+	
+}
